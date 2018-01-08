b7c7ad0cb60892d039559a926beca1567dc68f6e
lyk <liuyk@sysware.com.cn>
2018-01-08 23:57:44 +0800

    添加运动员

--- src/main/java/com/sysware/cloud/service/SportsmanService.java
+++ src/main/java/com/sysware/cloud/service/SportsmanService.java
@@ -1,4 +1,8 @@
 package com.sysware.cloud.service;
 
+import com.sysware.cloud.dto.SportsmanDto;
+import com.sysware.cloud.entity.Sportsman;
+
 public interface SportsmanService {
+    Sportsman addSportsman(SportsmanDto sportsmanDto);
 }
--- src/main/java/com/sysware/cloud/web/SportsmanController.java
+++ src/main/java/com/sysware/cloud/web/SportsmanController.java
@@ -1,14 +1,30 @@
 package com.sysware.cloud.web;
 
+import com.sysware.cloud.dto.SportsmanDto;
+import com.sysware.cloud.entity.Sportsman;
+import com.sysware.cloud.service.SportsmanService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
 public class SportsmanController {
+
+    @Autowired
+    private SportsmanService sportsmanService;
 
     @GetMapping("sportsman.htm")
     public String sports(){
         return "sportsman";
     }
 
+    /**
+     * 增加
+     */
+    @PostMapping(value = "/sportsman")
+    public Sportsman addSportsman(SportsmanDto sportsmanDto){
+        return sportsmanService.addSportsman(sportsmanDto);
+    }
 }
