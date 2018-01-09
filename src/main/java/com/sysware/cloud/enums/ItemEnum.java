450471f27999e47cdd0fc7593db27629d03ae499
lyk <liuyk@sysware.com.cn>
2018-01-09 10:24:34 +0800

    冬奥会项目枚举类

---
+++ src/main/java/com/sysware/cloud/enums/ItemEnum.java
@@ -0,0 +1,30 @@
+package com.sysware.cloud.enums;
+
+
+public enum ItemEnum {
+
+    LUGE("雪车"),
+    BOBSLEIQH("钢架雪车"),
+    ALPINE_SKIING("高山滑雪"),
+    SKI_JUMPING("跳台滑雪"),
+    BIATHLON("冬季两项"),
+    CROSS_COUNTRY_SKIING("越野滑雪"),
+    SNOWBOARDING("单板滑雪"),
+    FREESTYLING("自由式滑雪"),
+    CURLING("冰壶"),
+    SPEED_SKATING("速滑"),
+    SHORT_TRACK_SPEEDING_SKATING("短道"),
+    FIGURE_SKATING("花样"),
+    ;
+
+    private String item;
+
+    ItemEnum(String item) {
+        this.item = item;
+    }
+
+
+    public String getItem() {
+        return item;
+    }
+}
