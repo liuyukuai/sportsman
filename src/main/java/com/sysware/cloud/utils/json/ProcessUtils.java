91d73edeffee6e867172f055e1399a6839c86f58
liuyk <liuyk@sysware.com.cn>
2018-01-09 18:51:00 +0800

    去掉多余的包引用

--- src/main/java/com/sysware/cloud/utils/json/ProcessUtils.java
+++ src/main/java/com/sysware/cloud/utils/json/ProcessUtils.java
@@ -1,8 +1,8 @@
 package com.sysware.cloud.utils.json;
 
-import org.apache.commons.beanutils.BeanUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 
 import java.util.ArrayList;
 import java.util.Collections;
